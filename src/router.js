426c54bf6cedf8b96341f24bd7b336eab50e2107
Michael <michael.pang@flywire.com>
2021-03-08 17:04:54 -0500

    Add gf_14 view for Gravity Forms
    Added a GravityForm view to support handling #gf_14 in the URL. Minor changes to styling to mirror Flywire (ie. adjusting font to Rubik)

--- src/router.js
+++ src/router.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import QuickPay from './views/QuickPay.vue'
 import Complete from './views/Complete.vue'
+import GravityForm from './views/GravityForm.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/',  name: 'quick-pay', component: QuickPay },
   { path: '/complete',  name: 'complete', component: Complete },
+  { path: '/gf_14',  name: 'gravity-form', component: GravityForm },
+
 ]
 
 const router = new VueRouter({
